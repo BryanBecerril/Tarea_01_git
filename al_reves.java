9e6d2950885b90446eafaf8d788e5455f9cd945d
Bryan <l24121371@morelia.tecnm.mx>
2024-11-07 13:10:42 -0600

    al_reves

---
+++ al_reves.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class al_reves {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String palabra = "Elon Musk";
+
+        System.out.println("Ingresa tu palabra");
+        String palabra_2 = input.nextLine();
+
+
+        StringBuilder palabraInvertida = new StringBuilder(palabra);
+        palabraInvertida.reverse();
+        palabraInvertida.toString();
+        System.out.println(palabraInvertida);
+
+        StringBuilder palabra_inversa = new StringBuilder(palabra_2);
+        palabra_inversa.reverse();
+        palabra_inversa.toString();
+        System.out.println(palabra_inversa);
+    }
+}
+
+
