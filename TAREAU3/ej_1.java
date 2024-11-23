747df8c45c52be9d95eb06b065cf4a8d01edc0df
Bryan <l24121371@morelia.tecnm.mx>
2024-11-23 17:11:11 -0600

    ready2serve

---
+++ TAREAU3/ej_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class ej_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int suma = 0;
+        System.out.println("¿Cuantos numeros deseas ingresar?");
+        int cant_num = input.nextInt();
+        System.out.println(" ");
+
+
+        int numeros[] = new int[cant_num];
+        if (cant_num <= 0){
+            System.out.println("Cantidad invalida. Intente de nuevo.");
+        } else{
+            for (int i = 0; i < cant_num; i++) {
+                System.out.println("ingrese el numero " + (i + 1) + " :");
+                numeros[i] = input.nextInt();
+                suma += numeros[i];
+            }
+            double promedio = (double) (suma / cant_num);
+            if (promedio > 7){
+                System.out.println("Promedio de : " + promedio + " Aprovado.");
+            } else{
+                System.out.println("Promedio de : " + promedio + " Reprovado.");
+
+            }
+        }
+
+
+    }
+}
