219200c66dd2c4882a6911b9ce72d86cc7f4dfc7
Bryan Becerril <l24121371@morelia.tecnm.mx>
2024-11-27 22:52:39 -0600

    eme

---
+++ tarea 4/promedio.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class promedio{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bienvenido.");
+        System.out.println(" ");
+        System.out.println("¿De cuántos estudiantes va a registar calificación?");
+        int estudiantes = input.nextInt();
+
+        double suma = 0;
+        double califs[] = new double[estudiantes];
+
+        for (int i = 0; i < estudiantes; i++) {
+            System.out.println("Ingrese las calificaciones del estudiante " + (i+1) );     
+            califs[i] = input.nextDouble();
+            suma += califs[i];
+        }
+        //prom 
+        double promedd = (double) ((suma) / estudiantes);
+        System.out.println("Promedio: " + promedd);
+        System.out.println(" ");
+        int esudiante_arriba = 0, estudiante_abajo = 0;
+        //System.out.println("test " + califs[0]);
+        for (int i = 0; i < estudiantes; i++) {
+            if (promedd < califs[i]){
+                esudiante_arriba++;
+            }else{
+                estudiante_abajo++;
+            }
+        }
+
+        System.out.println("Estudiantes arriba del promedio: " + esudiante_arriba);
+        System.out.println(" ");
+        System.out.println("Estudiantes abajo del promedio: " + estudiante_abajo);
+
+    }
+}
