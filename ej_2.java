5ec4f0d391da67bdf574cd1063bc6cee5fd55443
Bryan Becerril <l24121371@morelia.tecnm.mx>
2024-12-08 01:32:54 -0600

    BK_al_d

--- ej_2.java
+++ ej_2.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 public class ej_2 {
     public static void main(String[] args) {
-        System.out.println("Hello_world");
+        Scanner in = new Scanner(System.in);
         /*Crea un método llamado calcularMedia que reciba un arreglo de números 
         enteros y devuelva la media (promedio) de esos números. En el método main, crea un 
         arreglo de enteros, llama al método calcularMedia y muestra el resultado.
         */
+        System.out.println("Cuantos numeros");
+        int veces = in.nextInt();
+        double[] arreglo = new double[veces];
+        double suma = 0;
+        double prom = 0;
+
+        for (int i = 0; i < veces; i++) {
+            System.out.println("Ingresa el numero");
+            double numero = in.nextDouble();
+            arreglo[i] = numero;
+        }
+        System.out.println("El promedio es: " + sacar_media(arreglo, prom, suma));
+    }
+    private static double sacar_media(double[] arreglo, double prom, double suma){
+            for (int i = 0; i < arreglo.length; i++) {
+                suma += arreglo[i];
+            }
+            prom = suma / arreglo.length;
+        return prom;
     }
 }
--- ej_3.java
+++ ej_3.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
 public class ej_3 {
     public static void main(String[] args) {
-        /* */
+        Scanner in = new Scanner(System.in);
+        /*Crea un método llamado imprimirPositivos que reciba un arreglo de 
+        números enteros y muestre por consola solo aquellos números que sean positivos (mayores 
+        que cero). En el método main, crea un arreglo con varios números, llama al método 
+        imprimirPositivos y muestra los números positivos*/
+        System.out.println("Cuántos números");
+        int veces = in.nextInt();
 
-        //https://itecm.sharepoint.com/:b:/s/FundamentosdeProgramacin-20242/EUZ10eWg1ZtMt89caC38WeQBRomZoq59L1RPOfQK_z5fQg?e=d6mPBx
+        int[] arreglo = new int[veces];
+
+        for (int i = 0; i < veces; i++) {
+            System.out.println("Ingresa el número:");
+            arreglo[i] = in.nextInt();
+        }
+
+        System.out.println("Números positivos en la lista:");
+        imprimir_positivos(arreglo);
+
+    }
+
+    private static void imprimir_positivos(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] > 0) { 
+                System.out.print(arreglo[i] + " ");
+            }
+        }
     }
 }
