cab9e70c6df4f6e92e421a25c7b0b7e4b058925a
Bryan Becerril <l24121371@morelia.tecnm.mx>
2024-12-08 00:53:02 -0600

    oj_es_a

---
+++ ej_2.java
@@ -0,0 +1,9 @@
+public class ej_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello_world");
+        /*Crea un método llamado calcularMedia que reciba un arreglo de números 
+        enteros y devuelva la media (promedio) de esos números. En el método main, crea un 
+        arreglo de enteros, llama al método calcularMedia y muestra el resultado.
+        */
+    }
+}
---
+++ ej_3.java
@@ -0,0 +1,7 @@
+public class ej_3 {
+    public static void main(String[] args) {
+        /* */
+
+        //https://itecm.sharepoint.com/:b:/s/FundamentosdeProgramacin-20242/EUZ10eWg1ZtMt89caC38WeQBRomZoq59L1RPOfQK_z5fQg?e=d6mPBx
+    }
+}
